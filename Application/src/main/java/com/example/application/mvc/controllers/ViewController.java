8c12610b820095be1c27c7eb4b94e9f825154f2a
hyssayas <yH062021!@#>
2022-02-02 13:15:30 +0500

    test mvc controller.

---
+++ Application/src/main/java/com/example/application/mvc/controllers/ViewController.java
@@ -0,0 +1,13 @@
+package com.example.application.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*@Controller
+public class ViewController //implements MvcControllerBase
+{
+    @GetMapping(value = "/abc")
+    public String abc() {
+        return "abc";
+    }
+}*/
