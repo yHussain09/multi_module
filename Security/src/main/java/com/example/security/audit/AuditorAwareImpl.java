44f6c7012c2ba3ec01f512e5db96c6c31c44eab3
hyssayas <yH062021!@#>
2022-02-02 12:21:09 +0500

    add lombok.

--- Security/src/main/java/com/example/security/audit/AuditorAwareImpl.java
+++ Security/src/main/java/com/example/security/audit/AuditorAwareImpl.java
@@ -1,14 +1,26 @@
 package com.example.security.audit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@Slf4j
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return  Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        String auditor = "";
+        if(securityContext.getAuthentication() != null) {
+            auditor = securityContext.getAuthentication().getName();
+            log.debug("Auditor: {} found from Spring Security Context.", auditor);
+        }
+        else {
+            log.debug("Spring Security Context not found.");
+        }
+        return Optional.of(auditor);
     }
 }
