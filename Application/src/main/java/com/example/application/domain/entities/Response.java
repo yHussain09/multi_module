471c9782b2789fb319b91a167874c9333032c273
hyssayas <yH062021!@#>
2022-02-02 13:14:06 +0500

    generic response dto for student test module.

---
+++ Application/src/main/java/com/example/application/domain/entities/Response.java
@@ -0,0 +1,25 @@
+package com.example.application.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
+public class Response {
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?,?> data;
+}
