60830ca5b8caea5ccb6ed532955f5014b3799678
hyssayas <yH062021!@#>
2022-02-02 11:52:29 +0500

    Web MVC controller base will be implemented into near future.

---
+++ CoreJava/src/main/java/com/example/corejava/mvc/controllers/WebControllerBase.java
@@ -0,0 +1,15 @@
+package com.example.corejava.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+/*@RequestMapping(value = "/app")
+public  class WebControllerBase {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+
+
+}*/
