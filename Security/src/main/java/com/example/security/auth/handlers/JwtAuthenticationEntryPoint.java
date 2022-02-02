844d545cd5712aa56c89caaba8a9617c318b5510
hyssayas <yH062021!@#>
2022-02-02 13:12:45 +0500

    removed files.

--- Security/src/main/java/com/example/security/auth/handlers/JwtAuthenticationEntryPoint.java
+++
@@ -1,9 +0,0 @@
-package com.example.security.auth.handlers;
-
-/*@Component
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError( HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage() );
-    }
-}*/
